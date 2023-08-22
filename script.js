40584a093a826ee81fb551188f8a20aeda05a3d7
Joshua Bliek <jbliek213@yahoo.com>
2023-08-21 19:16:06 -0700

    bg random

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 console.log('gday');
 
+let roomCount = 0;
+
+var bgs = ["url(./img/background/dungeon_bg1.jpg)", "url(./img/background/dungeon_bg2.jpg)", 
+"url(./img/background/dungeon_bg3.jpg)","url(./img/background/dungeon_bg4.jpg)" , 
+"url(./img/background/dungeon_bg5.jpg)", "url(./img/background/pre_boss_room.jpg)"]
+
 var start = document.getElementById('start')
 
 function addFlash() {
@@ -29,15 +35,47 @@ function addNew(id1, id2){
 }
 
 
-async function fadeTitle(id) {
+function fadeTitle(id) {
     var x = document.getElementById(id);
     x.classList.add("fade");
     console.log('faded');
 
-    await setTimeout(() => {
+    setTimeout(() => {
         x.remove();
-        console.log('gone');
         addNew('win1', 'win2');
-        console.log('new')
     }, 1100);
+}
+
+function rolShad(elem){
+    elem.classList.add('rol-shad')
+}
+
+function remShad(elem){
+    elem.classList.remove('rol-shad')
+}
+
+function roll(id){
+    var dia = document.getElementById('dialogue')
+    var num = Math.round(Math.random()*(19) + 1);
+    id.innerText = num;
+    dia.innerText = 'Your attack deals ' + num + ' damage!';
+}
+
+function eqSel(elem){
+    elem.classList.add('equip-sel')
+}
+
+function move(id){
+    var x = document.getElementById('content3');
+    var butt = document.getElementById(id);
+    x.style.opacity = 0;
+    setTimeout(() => {
+        x.style.backgroundImage = bgs[Math.round(Math.random() * (bgs.length-1))];
+        butt.remove();
+        roomCount++;
+        console.log(roomCount)
+    }, 500)
+    setTimeout(() => {
+        x.style.opacity = 1;
+    }, 1000)
 }
