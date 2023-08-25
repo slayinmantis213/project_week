a6519dbb4565c9d89b9252c2cf3c5bda506865dc
Joshua Bliek <jbliek213@yahoo.com>
2023-08-24 19:50:43 -0700

    mvp +

--- script.js
+++ script.js
@@ -9,17 +9,35 @@ let bgs = ["url(./img/background/dungeon_bg1.jpg)", "url(./img/background/dungeo
     "url(./img/background/dungeon_bg9.jpg)"]
 
 // console.log(bgs.length)
+var startSwitch = true;
+
+var charSwitch = true;
+
+let switchRoll = false;
+
+let switchMove = true;
+
+let pTurn = false;
+
+let pChar = '';
+
+let charW = '';
+
+let pLife = 100;
+
+let pRoll = 0;
+
+let monCount = 0;
 
 // EFFECTS
 
-var start = document.getElementById('start')
 
-function addFlash() {
-    start.classList.add("flash");
+function addFlash(elem) {
+    elem.classList.add("flash");
 }
 
-function cutFlash() {
-    start.classList.remove("flash");
+function cutFlash(elem) {
+    elem.classList.remove("flash");
 }
 
 function lightUp(elem) {
@@ -30,6 +48,13 @@ function lightDown(elem) {
     elem.style.opacity = 0.3;
 }
 
+function mAtk(id) {
+    var a = document.getElementById(id);
+    a.classList.add('m-atk')
+    setTimeout(() => {
+        a.classList.remove('m-atk')
+    }, 100)
+}
 
 function rolShad(elem) {
     elem.classList.add('rol-shad')
@@ -39,8 +64,9 @@ function remShad(elem) {
     elem.classList.remove('rol-shad')
 }
 
-setInterval(addFlash, 600);
-setInterval(cutFlash, 1200);
+function eqSel(elem) {
+    elem.classList.add('equip-sel')
+}
 
 // TRANSITIONS
 
@@ -53,6 +79,10 @@ function addNew(id1, id2) {
 
 
 function fadeTitle(id) {
+    if (startSwitch == false){
+        return;
+    }
+    startSwitch = false;
     var x = document.getElementById(id);
     x.classList.add("fade");
     setTimeout(() => {
@@ -62,27 +92,33 @@ function fadeTitle(id) {
 }
 
 
-//CHARACTERS/PLAYER
-
-let pChar = '';
-
-let pTurn = true;
-
-let pLife = 100;
-
-let pRoll = 0;
-
-let switchRoll = true;
-
-let switchMove = true;
-
-function eqSel(elem) {
-    elem.classList.add('equip-sel')
+function charSelect(char, id, con) {
+    if (charSwitch == false){
+        return;
+    }
+    charSwitch = false;
+    switchRoll = false;
+    pTurn = false;
+    charW = con;
+    roomCount = 0;
+    pLife = 100;
+    var v = document.getElementById('win1');
+    var w = document.getElementById(id);
+    pChar = char;
+    v.innerHTML = w.innerHTML;
+    var dia = document.getElementById('dialogue');
+    dia.innerText = '...'
 }
 
-let currentE = '';
 
-let monCount = 0;
+function menu(){
+    var v = document.getElementById('win1');
+    var t = document.getElementById('title-frame');
+    startSwitch = true;
+    charSwitch = true;
+    v.innerHTML = t.innerHTML;
+}
+
 // encounters: [number of monsters, max monsters, containing element]
 
 const encount = {
@@ -100,6 +136,8 @@ const encount = {
     e12: [1, 1, 'e-frame12']
 }
 
+// COMBAT
+
 //!!monster stats: [current life, max life, dmg range]!!//
 
 const monsters = {
@@ -107,80 +145,31 @@ const monsters = {
     goblin2: [35, 35, 3],
     imp: [50, 50, 5],
     wraith: [40, 40, 7],
-    troll: [90, 90, 6],
-    iguana: [70, 70, 6],
-    infernal: [95, 95, 8],
-    horror: [100, 100, 7],
-    dragon: [250, 250, 10]
+    troll: [85, 85, 6],
+    iguana: [65, 65, 6],
+    infernal: [85, 85, 8],
+    horror: [95, 95, 7],
+    dragon: [200, 200, 11]
 }
-
-function mAtk(id) {
-    var a = document.getElementById(id);
-    a.classList.add('m-atk')
-    setTimeout(() => {
-        a.classList.remove('m-atk')
-    }, 100)
-}
-
 
 function monDie(id) {
     var c = document.getElementById('c-frame')
     var ded = document.getElementById(id);
     var vic = document.getElementById('vic-box');
-    console.log(monCount);
     if (monCount <= 0) {
+        if (roomCount >= 5){
+            vic = document.getElementById('clear-box')
+        }
         c.innerHTML = vic.innerHTML;
     }
     ded.remove()
 }
 
 function pDie() {
-    var x = document.getElementById('content3');
+    var x = document.getElementById(charW);
     var d = document.getElementById('coffin');
     x.innerHTML = d.innerHTML;
 }
-
-function getHit(bar, mon, id) {
-    if (pTurn == false) {
-        return;
-    }
-    pTurn = false;
-    var mLife = monsters[mon][0];
-    // pRoll = 100;
-    mLife -= pRoll;
-    if (mLife <= 0) {
-        mLife = 0;
-    }
-    monsters[mon][0] = mLife;
-    var mBar = document.getElementById(bar);
-    var b = (mLife * 100) / monsters[mon][1];
-    mBar.style.width = b + "%";
-    if (mLife == 0) {
-        monCount--;
-        monDie(id);
-        switchRoll = true;
-        return
-    }
-    var pBar = document.getElementById('p-life');
-    var mRoll = Math.round(Math.random() * (monsters[mon][2] - 1) + 1);
-    setTimeout(() => {
-        mAtk(id);
-        pBar.style.width = pLife + '%'
-    }, 300)
-    pLife -= mRoll;
-    if (pLife <= 0) {
-        pLife = 0;
-    }
-    if (pLife == 0) {
-        pDie();
-    }
-    switchRoll = true;
-}
-
-// console.log(mons);'e' + Math.round(Math.random()*(10) + 1)
-// mons = document.getElementById(encount[mons][2]);
-
-// COMBAT
 
 
 function roll(id) {
@@ -188,7 +177,7 @@ function roll(id) {
         return;
     }
     switchRoll = false;
-    var dia = document.getElementById('dialogue')
+    var dia = document.getElementById('dialogue');
     var num = Math.round(Math.random() * (19) + 1);
     id.innerText = num;
 
@@ -203,6 +192,67 @@ function roll(id) {
         dia.innerText = 'You rolled a ' + num + '!';
     }
     pTurn = true;
+}
+
+
+function getHit(bar, mon, id) {
+    if (pTurn == false) {
+        return;
+    }
+    pTurn = false;
+    let dia = document.getElementById('dialogue');
+    var pBar = document.getElementById('p-life');
+    var mLife = monsters[mon][0];
+    // pRoll = 100;
+    if (pChar == 'rogue'){
+        console.log('rog')
+        if (pRoll == 17 || pRoll == 18 || pRoll == 19){
+            pRoll = 20;
+        }
+    }
+    if (pRoll == 20){
+        pRoll = 25;
+    }
+    if (pChar == 'warrior'){
+        console.log('war')
+        pRoll += 2;
+    }
+    if (pChar == 'mage'){
+        console.log('mag')
+        if (pRoll == 1){
+            pLife -= 5;
+            pBar.style.width = pLife + '%';
+        }
+        pRoll += Math.round(Math.random()*4 + 1);
+    }
+    console.log(pRoll);
+    mLife -= pRoll;
+    dia.innerText = 'You deal ' + pRoll + ' damage!';
+    if (mLife <= 0) {
+        mLife = 0;
+    }
+    monsters[mon][0] = mLife;
+    var mBar = document.getElementById(bar);
+    var b = (mLife * 100) / monsters[mon][1];
+    mBar.style.width = b + "%";
+
+    if (mLife == 0) {
+        monCount--;
+        monDie(id);
+        switchRoll = true;
+        return
+    }
+
+    var mRoll = Math.round(Math.random() * (monsters[mon][2] - 1) + 1);
+    mAtk(id);
+    pLife -= mRoll;
+    console.log(pLife);
+    if (pLife <= 0) {
+        pLife = 0;
+        pDie();
+    }
+    pBar.style.width = pLife + '%'
+    switchRoll = true;
 }
 
 function heal(elem) {
@@ -223,8 +273,11 @@ function proceed(nav) {
 
 
 function move(id) {
+    if (switchMove == false){
+        return;
+    }
     switchMove = false;
-    var x = document.getElementById('content3');
+    var x = document.getElementById(charW);
     var c = document.getElementById('c-frame');
     var butt = document.getElementById(id);
     var enc = '';
@@ -240,26 +293,25 @@ function move(id) {
     setTimeout(() => {
         butt.remove();
         switchMove = true;
-        // roomCount = 2;
         if (roomCount == 1) {
             x.style.backgroundImage = bgs[Math.round(Math.random() * (bgs.length - 1))];
             enc = document.getElementById('e-frame1');
             monCount = 2;
-            currentE = 'e1';
+            // currentE = 'e1';
             c.innerHTML += enc.innerHTML;
         }
         else if (roomCount >= 5) {
             x.style.backgroundImage = "url(./img/background/prompt.jpg)";
             enc = document.getElementById(encount['e12'][2]);
             monCount = 1;
-            currentE = 'e12'
+            // currentE = 'e12'
             c.innerHTML += enc.innerHTML;
         }
         else if (roomCount == 4) {
             x.style.backgroundImage = "url(./img/background/pre.jpg)";
             mons = 'e' + Math.round(Math.random() * (2) + 9)
             monCount = encount[mons][0];
-            currentE = mons;
+            // currentE = mons;
             enc = document.getElementById(encount[mons][2]);
             c.innerHTML += enc.innerHTML;
         }
@@ -267,7 +319,7 @@ function move(id) {
             x.style.backgroundImage = bgs[Math.round(Math.random() * (bgs.length - 1))];
             mons = 'e' + Math.round(Math.random() * (2) + 6)
             monCount = encount[mons][0];
-            currentE = mons;
+            // currentE = mons;
             enc = document.getElementById(encount[mons][2]);
             c.innerHTML += enc.innerHTML;
         }
@@ -275,11 +327,11 @@ function move(id) {
             x.style.backgroundImage = bgs[Math.round(Math.random() * (bgs.length - 1))];
             mons = 'e' + Math.round(Math.random() * (3) + 2)
             monCount = encount[mons][0];
-            currentE = mons;
+            // currentE = mons;
             enc = document.getElementById(encount[mons][2]);
             c.innerHTML += enc.innerHTML;
         }
-        console.log(roomCount)
+        // console.log(roomCount)
     }, 700)
     setTimeout(() => {
         x.style.opacity = 1;
